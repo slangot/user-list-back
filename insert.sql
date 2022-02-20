67827e232dd85dfe54d11990dbe58f6e44d1df10
Shawn <shawn17@live.fr>
2022-02-20 09:34:34 +0100

    creation of insert sql file to insert some datas

---
+++ insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO `users` SET firstname='Emma', lastname='Watson', mail='emma.watson@gmail.com';
+INSERT INTO `users` SET firstname='Daniel', lastname='Radcliffe', mail='daniel.radcliffe@gmail.com';
+INSERT INTO `users` SET firstname='Rupert', lastname='Grint', mail='rupert.grint@gmail.com';
+INSERT INTO `users` SET firstname='Tom', lastname='Felton', mail='tom.felton@gmail.com';
+INSERT INTO `users` SET firstname='Bonnie', lastname='Wright', mail='bonnie.wright@gmail.com';
+INSERT INTO `users` SET firstname='Evanna', lastname='Lynch', mail='evanna.lynch@gmail.com';
+INSERT INTO `users` SET firstname='Alan', lastname='Rickman', mail='alan.rickman@gmail.com';
